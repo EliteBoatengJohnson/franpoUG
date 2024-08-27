b69495268763fb8e0f9f42461d4e7ad660319367
Boateng Johnson <80701309+EliteBoatengJohnson@users.noreply.github.com>
2024-08-27 16:22:29 +0000

    got the claim page working making modifications

--- UGfindme/UGfindme/urls.py
+++ UGfindme/UGfindme/urls.py
@@ -21,8 +21,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',include('shop.urls',namespace='shop')),
     path('claim/', include('claim.urls',namespace='claim')),
+    path('',include('shop.urls',namespace='shop')),
+    
 ]
 
 if settings.DEBUG:
